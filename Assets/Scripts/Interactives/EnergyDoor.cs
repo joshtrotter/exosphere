15f1c443f93ab3dceb7ec50f8a8a5051f8c7a5b8
Michael Trotter <michaeltr42@gmail.com>
2015-12-16 12:59:00 +1000

    - Reprefabbed ramps with white hexes  - Fixed irregularities in tiling of corner floor pieces  - Adjusted normal and occlusion maps for toothed rail  - Energy door frame emission now turns on and off with field

--- Assets/Scripts/Interactives/EnergyDoor.cs
+++ Assets/Scripts/Interactives/EnergyDoor.cs
@@ -8,11 +8,21 @@ public class EnergyDoor : Door {
 	public ParticleSystem EnergyDoorFrontHit; 
 	public ParticleSystem EnergyDoorBackHit;
 
+	//store references to each piece of the side frame
+	public GameObject frame;
+	private Renderer[] side_pieces;
+
 	private Collider coll;
+
+	//store the initial 'On' emission color
+	private Color startColor;
 
 
 	void Awake(){
 		coll = GetComponent<Collider>();
+		side_pieces = frame.GetComponentsInChildren<Renderer> ();
+		startColor = side_pieces[0].material.GetColor ("_EmissionColor");
+		IsClosed = true;
 		IsClosed = !IsClosed;
 		SwapState ();
 	}
@@ -33,7 +43,10 @@ public class EnergyDoor : Door {
 	{
 		EnergyDoorFront.Play ();
 		EnergyDoorBack.Play ();
+
 		coll.enabled = true;
+		SetEmission (startColor);
+
 		IsClosed = true;
 	}
 
@@ -43,7 +56,17 @@ public class EnergyDoor : Door {
 		EnergyDoorBack.Clear ();
 		EnergyDoorFront.Stop ();
 		EnergyDoorBack.Stop ();
+
 		coll.enabled = false;
+		SetEmission (Color.black);
+
 		IsClosed = false;
+	}
+
+	//this function cycles through all pieces of the frame and changes their emission color
+	public void SetEmission(Color color){
+		foreach (Renderer side in side_pieces) {
+			side.material.SetColor ("_EmissionColor", color);
+		}
 	}
 }
