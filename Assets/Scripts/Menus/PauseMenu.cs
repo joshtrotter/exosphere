756f32422d87bdb5b836299475c246421c77614b
Michael Trotter <michaeltr42@gmail.com>
2015-12-30 22:48:23 +1000

    Added calibration (with one 'l') screen and a pause menu

---
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+	public GameObject pauseButton;
+	public GameObject unpauseButton;
+	public GameObject pauseMenu;
+	public CallibrationUI callibrator;
+
+	void Awake(){
+		unpauseButton.SetActive (false);
+		pauseMenu.SetActive (false);
+	}
+
+	public void Pause()
+	{
+		pauseButton.SetActive (false);
+		unpauseButton.SetActive (true);
+		pauseMenu.SetActive (true);
+		Time.timeScale = 0f;
+	}
+
+	public void Unpause()
+	{
+		pauseButton.SetActive (true);
+		unpauseButton.SetActive (false);
+		pauseMenu.SetActive (false);
+		Time.timeScale = 1f;
+	}
+
+	public void Recallibrate(){
+		Unpause ();
+		callibrator.StartCalibration ();
+	}
+}
