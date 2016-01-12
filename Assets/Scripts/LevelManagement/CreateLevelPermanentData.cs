4635f1b9a943b7c83011caf6e6626907807bb673
Michael Trotter <michaeltr42@gmail.com>
2016-01-12 20:02:48 +1000

    Changed tiltInput script to avoid axis error. Made CreateLevelPermanentData script only exist in the editor to stop a building compile bug

--- Assets/Scripts/LevelManagement/CreateLevelPermanentData.cs
+++ Assets/Scripts/LevelManagement/CreateLevelPermanentData.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEngine;
 using System.Collections;
 using UnityEditor;
 
@@ -20,3 +21,4 @@ public class CreateLevelPermanentData : MonoBehaviour {
 		Selection.activeObject = asset;
 	}
 }
+#endif
