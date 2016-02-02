13beba9919cbf2b43da84c7b57d6d532b03f89f3
Michael Trotter <michaeltr42@gmail.com>
2016-02-02 22:15:34 +1000

    Light ball now pops in lasers. Lower force required to shatter crystal ball

--- Assets/Scripts/Transforms/LightTransform.cs
+++ Assets/Scripts/Transforms/LightTransform.cs
@@ -2,5 +2,8 @@
 using System.Collections;
 
 public class LightTransform : BallTransform {
-	
+
+	public override void OnLaserEnter(LaserDiffuser laserDiffuser, ArcReactorHitInfo hitInfo){
+		GameObject.FindGameObjectWithTag("Player").GetComponent<BallDestroyer>().Pop();
+	}
 }
