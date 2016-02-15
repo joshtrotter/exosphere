18ad6c3093e53969e0361a52f60f56a2ccff226f
Michael Trotter <michaeltr42@gmail.com>
2016-02-15 21:41:33 +1000

    Improved vertical cannon camera movement. Added a new section to Into the Storm

---
+++ Assets/VerticalCannonLanding.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This scripte helps the vertical cannon to re-enable ball control and wall clipping control at the correct time
+// It should be placed on a trigger object just above where the ball will land
+public class VerticalCannonLanding : MonoBehaviour {
+
+	private VerticalCannon cannon;
+
+	void Awake(){
+		cannon = GetComponentInParent<VerticalCannon> ();
+	}
+
+	void OnTriggerEnter(Collider coll){
+		if (coll.CompareTag ("Player")) {
+			cannon.SetHasLanded();
+		}
+	}
+}
