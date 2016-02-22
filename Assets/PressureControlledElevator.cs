7b037bdbc6dac422b9b11f01930919b77f01033d
Michael Trotter <michaeltr42@gmail.com>
2016-02-22 22:27:15 +1300

    Continued work on Into the Storm

--- Assets/PressureControlledElevator.cs
+++ Assets/PressureControlledElevator.cs
@@ -3,31 +3,33 @@ using System.Collections;
 
 public class PressureControlledElevator : PressureReceiver {
 	
-	public float height = 4f;
+	//public float height = 4f;
 	public Vector3 posChange = new Vector3 (0, 4, 0);
-	public bool pressureToRaise = true;
+	//public bool pressureToRaise = true;
 
-	private float baseY;
+	//private float baseY;
 	private Vector3 startPos;
 
 	void Awake() {
-		this.baseY = transform.position.y;
+		//this.baseY = transform.position.y;
 		startPos = transform.position;
 	}
 
 	public override void Apply (float pressureAmount)
 	{
-		Vector3 newPos = transform.position;
+		/*Vector3 newPos = transform.position;
 		//newPos.y = baseY + Mathf.Clamp01 (pressureAmount) * height * (pressureToRaise ? 1 : -1);
-		newPos = startPos + Mathf.Clamp01 (pressureAmount) * posChange;
-		transform.position = newPos;
+		newPos = */
+		transform.position = startPos + Mathf.Clamp01 (pressureAmount) * posChange;
 	}
 
-	void OnCollisionStay(Collision coll){
+	void OnTriggerStay(Collider coll){
+		Debug.Log ("Staying in trigger");
 		coll.transform.parent = this.transform;
 	}
 
-	void OnCollisionExit(Collision coll){
+	void OnTriggerExit(Collider coll){
+		Debug.Log ("exiting trigger");
 		coll.transform.parent = null;
 	}
 }
