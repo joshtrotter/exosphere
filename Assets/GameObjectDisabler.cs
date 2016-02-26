5f36b4365c4bef3464d88d563fa8eea714a45c48
Michael Trotter <michaeltr42@gmail.com>
2016-02-26 17:21:21 +1300

    Into the Storm now disables/enables required sections and has varied weather effects. Changed trigger size on zoompad to be more streamlined.

---
+++ Assets/GameObjectDisabler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameObjectDisabler : SwitchableObject {
+
+	public GameObject targetObject;
+
+	public override void Activate ()
+	{
+		Debug.Log ("Activated");
+		if (targetObject.activeSelf) {
+			Debug.Log ("Setting object " + targetObject.name + " inactive");
+			targetObject.SetActive (false);
+		} else {
+			Debug.Log ("Setting object " + targetObject.name + " active");
+			targetObject.SetActive(true);
+		}
+	}
+}
