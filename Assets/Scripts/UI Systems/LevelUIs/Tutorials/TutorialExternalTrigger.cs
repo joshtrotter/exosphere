a1359648a88db22f4497d7b0e717771ca136d0c0
jtrotter <jtrotter@onetest.com.au>
2016-03-06 08:14:03 +1000

    Updated tutorial external trigger so it can be used on trigger exit. Updated tutes in light level to use triggers

--- Assets/Scripts/UI Systems/LevelUIs/Tutorials/TutorialExternalTrigger.cs
+++ Assets/Scripts/UI Systems/LevelUIs/Tutorials/TutorialExternalTrigger.cs
@@ -5,12 +5,23 @@ using System.Collections;
 public class TutorialExternalTrigger : MonoBehaviour {
 
 	public TutorialMessage.TutorialSwitchTuple[] tutorials;
+	public bool exitTrigger = false;
 
 	void OnTriggerEnter(Collider coll){
-		if (coll.CompareTag("Player")){
-			foreach (TutorialMessage.TutorialSwitchTuple tutorial in tutorials){
-				tutorial.tut.ExternalTriggerCall(tutorial.method);
-			}
+		if (coll.CompareTag("Player") && !exitTrigger){
+			doTrigger ();
+		}
+	}
+
+	void OnTriggerExit(Collider coll){
+		if (coll.CompareTag("Player") && exitTrigger){
+			doTrigger ();
+		}
+	}
+
+	private void doTrigger() {
+		foreach (TutorialMessage.TutorialSwitchTuple tutorial in tutorials){
+			tutorial.tut.ExternalTriggerCall(tutorial.method);
 		}
 	}
 }
