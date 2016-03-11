4e2beef8c8e0a79a8cab5e6f8bbb2f9941f1ce3c
jtrotter <jtrotter@onetest.com.au>
2016-03-11 11:58:17 +1000

    Began work on the tunnel sections. Added some tunnel prefabs. Updated the light level to make use of the new camera manipulation features as well as the ball movement modifiers to help the light ball get over the see saw.

--- Assets/LightBallSeeSawBooster.cs
+++ Assets/LightBallSeeSawBooster.cs
@@ -1,34 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LightBallSeeSawBooster : MonoBehaviour {
+public class LightBallSeeSawBooster : BallMovementManipulator {
 
-	public float boostPower = 2f;
-
-	private float initialBoostPower;
-	private TransformController transformController;
-	private BallController ballController;
-
-	void Awake() {
-		transformController = GameObject.FindGameObjectWithTag ("Player").GetComponent<TransformController> ();
-		ballController = GameObject.FindGameObjectWithTag ("Player").GetComponent<BallController> ();
-		initialBoostPower = ballController.movementModifiers.movePowerScaler;
-	}
-
-	void OnTriggerEnter(Collider coll) {
-		if (coll.CompareTag ("Player") && ShouldBoostBall()) {
-			initialBoostPower = ballController.movementModifiers.movePowerScaler;
-			ballController.movementModifiers.movePowerScaler = boostPower;
-		}
-	}
-	
-	void OnTriggerExit(Collider coll) {
-		if (coll.CompareTag ("Player")) {
-			ballController.movementModifiers.movePowerScaler = initialBoostPower;
-		}
-	}
-
-	private bool ShouldBoostBall() {
-		return transformController.currentTransform.GetType () == typeof(LightTransform);
+	protected override bool ShouldModify(GameObject ball) {
+		return ball.GetComponent<TransformController>().currentTransform.GetType () == typeof(LightTransform);
 	}
 }
