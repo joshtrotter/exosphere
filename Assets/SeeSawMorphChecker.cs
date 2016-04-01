1e4fbb484cc9c35aa9db2d3df993b89764bb6287
jtrotter <jtrotter@onetest.com.au>
2016-04-01 17:18:40 +1000

    Fixed a bug where the shaking the light morph off on the seesaw could leave the seesaw with the weight bias at the wrong end

---
+++ Assets/SeeSawMorphChecker.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeeSawMorphChecker : MonoBehaviour {
+
+	public SeeSawController seeSawController;
+
+	private TransformController transformController;
+	private Coroutine coroutine;
+
+	void Awake() {
+		transformController = GameObject.FindGameObjectWithTag ("Player").GetComponent<TransformController> ();
+	}
+
+	void OnTriggerEnter(Collider coll) {
+		if (coll.CompareTag ("Player") && IsLightBall()) {
+			coroutine = StartCoroutine(CheckForLossOfMorph(coll));
+		}
+	}
+	
+	void OnTriggerExit(Collider coll) {
+		if (coll.CompareTag ("Player")) {
+			if (coroutine != null) {
+				StopCoroutine(coroutine);
+				coroutine = null;
+			}
+		}
+	}
+
+	private IEnumerator CheckForLossOfMorph(Collider coll) {
+		while (IsLightBall ()) {
+			yield return new WaitForEndOfFrame();
+		}
+		seeSawController.ResetBias(coll);
+	}
+
+	private bool IsLightBall() {
+		return transformController.currentTransform.GetType () == typeof(LightTransform);
+	}
+}
