bc4c024328918d38c03250422daed383c49ebeea
jtrotter <jtrotter@onetest.com.au>
2016-04-27 17:34:28 +1000

    Added a hole piece with randomised variations, more work on the procedural generation setup, added a score tracker and added it as a level in the UI

---
+++ Assets/Scripts/Tunnels/JumpTunnelPiece.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpTunnelPiece : TunnelPiece {
+	
+	public float maxJumpExtension = 20f;
+
+	public override void setup (TunnelSelectionPreferences prefs, TunnelPiece parent)
+	{
+		base.setup (prefs, parent);
+		float jumpExtension = Mathf.Clamp ((TunnelSpawnController.INSTANCE.getCurrentClearRun () - this.minClearSequenceBefore) / 3, 0, maxJumpExtension);
+		this.endOffset = this.endOffset + (Vector3.forward * jumpExtension);
+	}
+}
