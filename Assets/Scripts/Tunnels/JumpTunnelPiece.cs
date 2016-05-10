4d069c1d82b9d61eb2ecbf3a890e95fbc43d1b26
jtrotter <jtrotter@onetest.com.au>
2016-05-10 15:56:39 +1000

    Fixed a bug where the jump extension was not reset each time the piece was selected

--- Assets/Scripts/Tunnels/JumpTunnelPiece.cs
+++ Assets/Scripts/Tunnels/JumpTunnelPiece.cs
@@ -6,11 +6,20 @@ public class JumpTunnelPiece : TunnelPiece {
 	public float maxJumpExtension = 20f;
 	public Transform velocityBraker;
 
+	private float jumpExtension;
+
 	public override void setup (TunnelSelectionPreferences prefs, TunnelPiece parent)
 	{
 		base.setup (prefs, parent);
-		float jumpExtension = Mathf.Clamp (((TunnelSpawnController.INSTANCE.getCurrentClearRun())) / 4, 0, maxJumpExtension);
+		jumpExtension = Mathf.Clamp (((TunnelSpawnController.INSTANCE.getCurrentClearRun())) / 4, 0, maxJumpExtension);
 		velocityBraker.position = velocityBraker.position + (Vector3.forward * jumpExtension); 
 		this.endOffset = this.endOffset + (Vector3.forward * jumpExtension);
+	}
+
+	public override void tearDown ()
+	{
+		base.tearDown ();
+		velocityBraker.position = velocityBraker.position + (Vector3.back * jumpExtension); 
+		this.endOffset = this.endOffset + (Vector3.back * jumpExtension);
 	}
 }
