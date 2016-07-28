a5df8465eead7afea8cbcaca0d517fa4233cf16d
Michael Trotter <michaeltr42@gmail.com>
2016-07-28 16:35:57 +1200

    The ends of jump are now raised somewhat - dependant on their extension depth - to try to avoid overshooting. Removed 2x multiplier pickup from the pickup spawner for now, too much unnecessary work

--- Assets/Scripts/Tunnels/JumpTunnelPiece.cs
+++ Assets/Scripts/Tunnels/JumpTunnelPiece.cs
@@ -3,23 +3,29 @@ using System.Collections;
 
 public class JumpTunnelPiece : TunnelPiece {
 	
-	public float maxJumpExtension = 20f;
+	public float maxJumpExtension = 35f;
+	public float maxJumpRise = 5f;
 	public Transform velocityBraker;
 
 	private float jumpExtension;
+	private float jumpRise;
 
 	public override void setup (TunnelSelectionPreferences prefs, TunnelPiece parent)
 	{
 		base.setup (prefs, parent);
 		jumpExtension = Mathf.Clamp (((TunnelSpawnController.INSTANCE.getCurrentClearRun())) / 4, 0, maxJumpExtension);
-		velocityBraker.position = velocityBraker.position + (Vector3.forward * jumpExtension); 
+		jumpRise = Mathf.Lerp (0, maxJumpRise, jumpExtension / maxJumpExtension);
+		Debug.Log ("Jump rise = " + jumpRise + ", Jump extension = " + jumpExtension);
+		velocityBraker.position = velocityBraker.position + (Vector3.forward * jumpExtension) + (Vector3.up * jumpRise); 
 		this.endOffset = this.endOffset + (Vector3.forward * jumpExtension);
+		this.endOffset = this.endOffset + (Vector3.up * jumpRise);
 	}
 
 	public override void tearDown ()
 	{
 		base.tearDown ();
-		velocityBraker.position = velocityBraker.position + (Vector3.back * jumpExtension); 
+		velocityBraker.position = velocityBraker.position + (Vector3.back * jumpExtension) + (Vector3.down * jumpRise); 
 		this.endOffset = this.endOffset + (Vector3.back * jumpExtension);
+		this.endOffset = this.endOffset + (Vector3.down * jumpRise);
 	}
 }
