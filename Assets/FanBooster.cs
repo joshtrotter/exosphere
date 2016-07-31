7965dc2a7c73b239b24293f90ab7f8b0cc194298
Michael Trotter <michaeltr42@gmail.com>
2016-07-31 16:42:14 +1200

    Various adjustments to level 4, mostly around the jumps

--- Assets/FanBooster.cs
+++ Assets/FanBooster.cs
@@ -6,7 +6,8 @@ public class FanBooster : MonoBehaviour {
 	public float fanBoostPower = 5f;
 
 	void OnTriggerEnter(Collider coll) {
-		if (coll.gameObject.GetComponent<TransformController> ().currentTransform.GetType() == typeof(LightTransform)) {
+		TransformController ballTransform = coll.gameObject.GetComponent<TransformController> ();
+		if (ballTransform != null && ballTransform.currentTransform.GetType() == typeof(LightTransform)) {
 			coll.attachedRigidbody.velocity *= 0.1f; 
 		}
 		coll.attachedRigidbody.AddForce (Vector3.up * fanBoostPower, ForceMode.Impulse);
