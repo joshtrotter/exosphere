c2b05c13c7fb63fb9fca539d4992cb669a816db2
Michael Trotter <michaeltr42@gmail.com>
2016-08-13 14:29:54 +1200

    Added a twisting laser tunnel piece. Lots of tunnel runner tweaks, incl. a slightly reduced max jump extension

--- Assets/Scripts/Utils/AxisRotator.cs
+++ Assets/Scripts/Utils/AxisRotator.cs
@@ -5,9 +5,16 @@ public class AxisRotator : MonoBehaviour {
 
 	public float rotationsPerSecond = 1f;
 	public Vector3 rotateVector = Vector3.up;
+	public bool randomiseDirection = false;
 
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate (rotateVector * rotationsPerSecond * 360f * Time.deltaTime);
+	}
+
+	void OnEnable(){
+		if (randomiseDirection) {
+			rotateVector *= (Random.Range (0,1) == 1 ? 1 : -1);
+		}
 	}
 }
