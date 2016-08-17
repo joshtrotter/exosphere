fe82324bed40aa3a2b982d3427941929d07c3ba3
Michael Trotter <michaeltr42@gmail.com>
2016-08-17 22:48:30 +1200

    Added a new tunnel piece

---
+++ Assets/AxisOscillator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AxisOscillator : MonoBehaviour {
+
+	public Vector3 endRotation;
+	public float frequency = 1f;
+
+	void FixedUpdate() {
+		transform.localEulerAngles = endRotation * Mathf.Sin (Time.time * Mathf.PI / frequency);
+	}
+}
