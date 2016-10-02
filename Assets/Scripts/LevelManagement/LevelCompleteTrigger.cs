86974fc46fdf1100dee8079be026138638d99adb
Michael Trotter <michaeltr42@gmail.com>
2016-10-03 12:31:57 +1300

    Changed the end of the lvl 6 tunnel to look cooler. Fixed lvl 4

--- Assets/Scripts/LevelManagement/LevelCompleteTrigger.cs
+++ Assets/Scripts/LevelManagement/LevelCompleteTrigger.cs
@@ -4,14 +4,19 @@ using System.Collections;
 public class LevelCompleteTrigger : MonoBehaviour {
 	
 	public float delay = 0f;
+	public bool freezeCam = false;
 
 	void OnTriggerEnter(Collider coll){
 		if (coll.CompareTag("Player")){
-			StartCoroutine (CompleteLevel());
+			StartCoroutine (CompleteLevel(coll));
 		}
 	}
 
-	private IEnumerator CompleteLevel(){
+	private IEnumerator CompleteLevel(Collider ball){
+		if (freezeCam) {
+			AmazeballCam cam = GameObject.FindGameObjectWithTag("CameraRig").GetComponent<AmazeballCam>();
+			cam.moveSpeed = 0f;
+		}
 		yield return new WaitForSeconds(delay);
 		LevelCompleteScreen.controller.LevelComplete ();
 	}
