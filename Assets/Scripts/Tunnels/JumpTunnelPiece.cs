16d50b891bddf84134e4f2ff26038fc41a909945
Michael Trotter <michaeltr42@gmail.com>
2016-11-07 23:53:06 +1300

    Added more tips to the tunnerl runner load screen. Added a barrier above the tunnel at the end of a jump to aid in preventing the ball from ending up on the roof

--- Assets/Scripts/Tunnels/JumpTunnelPiece.cs
+++ Assets/Scripts/Tunnels/JumpTunnelPiece.cs
@@ -5,7 +5,7 @@ public class JumpTunnelPiece : TunnelPiece {
 	
 	public float maxJumpExtension = 35f;
 	public float maxJumpRise = 5f;
-	public Transform velocityBraker;
+	public Transform jumpEnd;
 
 	private float jumpExtension;
 	private float jumpRise;
@@ -17,7 +17,7 @@ public class JumpTunnelPiece : TunnelPiece {
 		float difficultyBasedMaxJumpRise = Mathf.Lerp (0, maxJumpRise, (1 - (2 * TunnelSpawnController.INSTANCE.GetCurrentAverageDifficulty ())));
 		jumpRise = Mathf.Lerp (0, difficultyBasedMaxJumpRise, jumpExtension / maxJumpExtension);
 		Debug.Log ("Jump rise = " + jumpRise + ", Jump extension = " + jumpExtension + ", Ave Difficulty = " + TunnelSpawnController.INSTANCE.GetCurrentAverageDifficulty());
-		velocityBraker.position = velocityBraker.position + (Vector3.forward * jumpExtension) + (Vector3.up * jumpRise); 
+		jumpEnd.position = jumpEnd.position + (Vector3.forward * jumpExtension) + (Vector3.up * jumpRise); 
 		this.endOffset = this.endOffset + (Vector3.forward * jumpExtension);
 		this.endOffset = this.endOffset + (Vector3.up * jumpRise);
 	}
@@ -25,7 +25,7 @@ public class JumpTunnelPiece : TunnelPiece {
 	public override void tearDown ()
 	{
 		base.tearDown ();
-		velocityBraker.position = velocityBraker.position + (Vector3.back * jumpExtension) + (Vector3.down * jumpRise); 
+		jumpEnd.position = jumpEnd.position + (Vector3.back * jumpExtension) + (Vector3.down * jumpRise); 
 		this.endOffset = this.endOffset + (Vector3.back * jumpExtension);
 		this.endOffset = this.endOffset + (Vector3.down * jumpRise);
 	}
---
+++ Assets/Scripts/Tunnels/TunnelJumpSwatBarrier.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TunnelJumpSwatBarrier : MonoBehaviour {
+	
+	//store the initial 'On' emission color
+	public Color onColor = new Color(0xD3F,0x1CF,0xE2F);
+	
+	public ParticleSystem EnergyBarrier; 
+	public ParticleSystem EnergyBarrierHit; 
+	
+	//store references to each piece of the side frame
+	public GameObject frame;
+	private Renderer[] side_pieces;
+
+	
+	void Awake(){
+		side_pieces = frame.GetComponentsInChildren<Renderer> ();
+		//set colour of energy field correctly
+		EnergyBarrier.GetComponent<Renderer>().material.SetColor ("_Color", onColor);
+		EnergyBarrierHit.GetComponent<Renderer>().material.SetColor ("_Color", onColor);
+		SetEmissionColor (onColor);
+
+		EnergyBarrier.Play ();
+
+	}
+	
+	void OnCollisionEnter(){
+		EnergyBarrierHit.Play ();
+	}
+	
+	//this function cycles through all pieces of the frame and changes their emission color
+	public void SetEmissionColor(Color color){
+		foreach (Renderer side in side_pieces) {
+			side.material.SetColor ("_EmissionColor", color);
+		}
+	}
+}
