05c91bc24af6084d34d1412ebf4e9c6f865564d9
Michael Trotter <michaeltr42@gmail.com>
2016-11-18 22:40:23 +1300

    Re-did some lighting in level 6

---
+++ Assets/FlashingLight.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashingLight : MonoBehaviour {
+
+	public float period = 1f;
+	//on percentage of period, between 0 and 1
+	public float dutyCycle = 0.5f;
+
+	private Light light;
+	private Color lightColour;
+	private Renderer rend;
+
+	void OnEnable(){
+		light = GetComponent<Light> ();
+		lightColour = light.color;
+		rend = this.gameObject.GetComponent<Renderer> ();
+		StartCoroutine (Flash ());
+	}
+
+	private IEnumerator Flash(){
+		while (isActiveAndEnabled) {
+			light.enabled = true;
+			rend.material.SetColor ("_EmissionColor", lightColour);
+			yield return new WaitForSeconds(dutyCycle/period);
+			light.enabled = false;
+			rend.material.SetColor ("_EmissionColor", Color.black);
+			yield return new WaitForSeconds((1-dutyCycle)/period);
+		}
+	}
+}
---
+++ Assets/WallLight.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallLight : MonoBehaviour {
+
+	void Awake(){
+		Color lightColour = GetComponentInChildren<Light> ().color;
+		this.gameObject.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", lightColour);
+	}
+}
