184bec74836aaad45087363e91bc46f745d7114a
Michael Trotter <michaeltr42@gmail.com>
2016-12-06 21:24:59 +1300

    Gave Calamity an atmoshperic overhaul fit for its name

---
+++ Assets/Scripts/Utils/SkyboxRotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyboxRotator : MonoBehaviour {
+
+	private Material skybox;
+	public float rotateSpeed = 0.2f;
+	public bool rotateClockwise = false;
+
+	void Awake(){
+		skybox = RenderSettings.skybox;
+	}
+	
+	void FixedUpdate()
+	{
+		skybox.SetFloat("_Rotation", (Time.time * rotateSpeed * (rotateClockwise ? -1 : 1)) % 360);
+	}
+}
