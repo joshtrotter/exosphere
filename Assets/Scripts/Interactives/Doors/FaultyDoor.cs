55d93b3b07d7c13184ad7ec50510225eba36338d
jtrotter <jtrotter@onetest.com.au>
2016-12-27 19:32:24 +1000

    Updated Level 6. Reworked the faulty doors and the robot rooms

--- Assets/Scripts/Interactives/Doors/FaultyDoor.cs
+++ Assets/Scripts/Interactives/Doors/FaultyDoor.cs
@@ -3,78 +3,104 @@ using UnityEngine;
 using System.Collections;
 using DG.Tweening;
 
-public class FaultyDoor : MoveBetweenTargets {
+public class FaultyDoor : MonoBehaviour {
 
-	float targetY;
-	float fullOpenY;
-	float fullCloseY;
-	bool goingUp = true;
-	int movementCounter = 0;
+
+
+	public float maxTimeOpen = 2.0f;
+	public float minTimeOpen = 1.0f;
+	public float triggerWarningTime = 0.25f;
+
+	public float minWobbleHeight = 1.5f;
+	public float maxWobbleHeight = 2.25f;
+	public float openHeight = 2.70f;
+
+	public float wobbleWaitTime = 0.5f;
+	public float wobbleDropTime = 0.2f;
+	public float wobbleRaiseTime = 0.5f;
+	public float shutWaitTime = 0.5f;
+	public float shutDropTime = 0.4f;
+	public float shutRaiseTime = 1f;
+
+	public int percentChanceOfSlam = 50;
+
+	private float bottomY;
+	
 	ParticleSystem[] sparks;
 
-	protected override void Awake ()
-	{
-		fullCloseY = transform.position.y;
-		fullOpenY = fullCloseY + targetPositions [0].y;
-		targetY = fullOpenY;
+	void Awake () {
+		bottomY = transform.position.y;
 		sparks = GetComponentsInChildren<ParticleSystem> ();
-
 	}
 
-	protected override Tween MoveToTarget (Vector3 target)
-	{
-		return transform.DOMove (target, targetY == fullCloseY ? 0.5f : Random.Range (0.5f, timeToMove));
+	void Start () {
+		transform.DOMoveY (bottomY + openHeight, shutDropTime).Play();
+		ScheduleAction ();
 	}
 
-	protected override Vector3 FindNewTarget ()
-	{
-		setRandomInterval ();
-		if (movementCounter % 6 == 0) {
-			setFullCloseToPoint ();
-		} else if (movementCounter % 2 == 0) {
-			setRandomOpenToPoint ();
-		} else {
-			setRandomCloseToPoint ();
+	private void ScheduleAction() {
+		if (Random.Range (1, 101) <= percentChanceOfSlam) {
+			SlamShut();
+		} else { 
+			Wobble();
 		}
-		goingUp = !goingUp;
-		movementCounter++;
-		return new Vector3 (transform.position.x, targetY, transform.position.z);
 	}
 
-	protected override Tween DoTargetAction ()
-	{
-		sparks [Random.Range (0, sparks.Length-1)].Play ();
-		return null;
+	private void Wobble() {
+		SequenceWobble ().OnComplete (ScheduleAction);
+	}
+
+	private void SlamShut() {
+		SequenceSlamShut ().OnComplete (ScheduleAction);
+	}
+	
+	private Tween SequenceWobble() {	
+		//Move the door to open, wait a while, then trigger the next action
+		Sequence sequence = DOTween.Sequence ()
+			.AppendInterval (Random.Range (minTimeOpen, maxTimeOpen))
+				.AppendCallback (TriggerWarning)
+				.AppendInterval (triggerWarningTime)
+				.Append (MoveToTarget (Random.Range (bottomY + minWobbleHeight, bottomY + maxWobbleHeight), wobbleDropTime))
+				.AppendInterval (wobbleWaitTime)
+				.Append (MoveToTarget(bottomY + openHeight, wobbleRaiseTime));
+			
+		return sequence;
+	}
+
+	private Tween SequenceSlamShut() {	
+		//Move the door to open, wait a while, then trigger the next action
+		Sequence sequence = DOTween.Sequence ()
+			.AppendInterval (Random.Range (minTimeOpen, maxTimeOpen))
+				.AppendCallback (TriggerWarning)
+				.AppendInterval (triggerWarningTime)
+				.Append (MoveToTarget (bottomY, shutDropTime))
+				.AppendInterval (shutWaitTime)
+				.Append (MoveToTarget(bottomY + openHeight, shutRaiseTime));
+		
+		return sequence;
+	}
+
+	private void TriggerWarning() {
+		Sparks (1);
+	}
+
+	private Tween MoveToTarget (float targetY, float time) {
+		Sparks (1);
+		return transform.DOMoveY (targetY, time);
+	}
+
+	private void Sparks(int howMany) {
+		for (int i = 0; i < howMany; i++) {
+			sparks [Random.Range (0, sparks.Length-1)].Play ();
+		}
 	}
 
 	void OnTriggerEnter(Collider coll) {
 		if (coll.CompareTag ("Player")) {
 			coll.GetComponent<BallDestroyer>().Crush ();
 			DOTween.KillAll ();
-			transform.DOMove (new Vector3(transform.position.x, fullCloseY, transform.position.z), 0.2f).Play ();
+			transform.DOMove (new Vector3(transform.position.x, bottomY, transform.position.z), 0.2f).Play ();
 		}
 	}
-
 	
-	protected override Tween LookAtTarget (Vector3 target)
-	{
-		return null;
-	}
-
-	
-	private void setRandomCloseToPoint() {
-		targetY = Random.Range (fullCloseY + 0.75f, targetY);
-	}
-	
-	private void setRandomOpenToPoint() {
-		targetY = Random.Range (targetY, fullOpenY);
-	}
-	
-	private void setFullCloseToPoint() {
-		targetY = fullCloseY;
-	}
-	
-	private void setRandomInterval () {
-		interval = Random.Range (0.5f * interval, interval);
-	}
 }
